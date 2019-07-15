ad3045e3c9cf8712a1f8789db3b2afd7f36ccfe7
Jonathan Morfin <jmorfin776@gmail.com>
2019-07-15 14:10:32 -0400

    Project basic layout complete, TODO: Add page content

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+
+#bootstrap requires app instance, always comes after app is declared
+bootstrap = Bootstrap(app)
+
+from app import routes
