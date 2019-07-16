a08ed0ba589dd4886a4263cc5195ce4a3801091c
Jonathan Morfin <jmorfin776@gmail.com>
2019-07-16 11:43:14 -0400

    Made Home page, checkout page

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,45 @@ from flask import render_template
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    products = [
+    {
+        'id': 1001,
+        'title': 'soap',
+        'price': '3.98',
+        'desc': 'Very clean soap'
+
+    },
+    {
+        'id': 1002,
+        'title': 'apples',
+        'price': '2.98',
+        'desc': 'granny smith'
+
+    },
+    {
+        'id': 1003,
+        'title': 'dishwasher',
+        'price': '3980.00',
+        'desc': 'cleans very dirty dishes'
+
+    },
+    {
+        'id': 1004,
+        'title': 'carrots',
+        'price': '1.50',
+        'desc': 'great for rabbits'
+
+    }
+    ]
+
+    return render_template('index.html', products = products, title='Home')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html', title='Checkout')
+
+@app.route('/title')
+def title():
+    return render_template('form.html', title='Change Title')
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from app import app
+
+
+
+#if you want to run multiple at once
+# if __name__ == '__main__':
+#     app
